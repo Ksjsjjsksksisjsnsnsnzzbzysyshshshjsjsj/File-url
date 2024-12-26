dac58a98b4666d801f89cef776afeefcb959cb61
rohit3607 <rohitplayer87089@gmail.com>
2024-12-26 21:40:45 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -4,23 +4,20 @@ import string
 import random
 from configs import Config
 from pyrogram import Client
+from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
-async def notify_user(bot: Client, user_id: int):
+async def reply_forward(message: Message, file_id: int):
     try:
-        await bot.send_message(
-            chat_id=user_id,
-            text="Your files have been delivered and will be deleted in 30 minutes. Please save them promptly.",
-            disable_web_page_preview=True
+        await message.reply_text(
+            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
+            disable_web_page_preview=True,
+            quote=True
         )
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        await bot.send_message(
-            chat_id=user_id,
-            text="Your files have been delivered and will be deleted in 30 minutes. Please save them promptly.",
-            disable_web_page_preview=True
-        )
+        await reply_forward(message, file_id)
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -32,14 +29,13 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
                                               message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return await media_forward(bot, user_id, file_id)
+        return media_forward(bot, user_id, file_id)
+        await message.delete()
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
     asyncio.create_task(delete_after_delay(sent_message, 1800))
-    
-    # Send a separate notification to the user
-    await notify_user(bot, user_id)
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
