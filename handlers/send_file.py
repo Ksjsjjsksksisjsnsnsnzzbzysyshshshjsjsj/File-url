96f4f83716ff1d8cfd78187ebff2289181631cbc
rohit3607 <rohitplayer87089@gmail.com>
2024-12-26 19:24:20 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -8,16 +8,6 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
-async def reply_forward(message: Message, file_id: int):
-    try:
-        await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
-            disable_web_page_preview=True,
-            quote=True
-        )
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        await reply_forward(message, file_id)
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -30,12 +20,30 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
-        await message.delete()
 
-async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+async def send_media(bot: Client, user_id: int, file_id: int):
+    # Forward media to the user
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
+    
+    
+    
+    # Schedule deletion after a delay
     asyncio.create_task(delete_after_delay(sent_message, 1800))
+    
+    # Send a wait message
+    try:
+        await sent_message.reply_text(
+            "Thank you for waiting! Your file is ready and will be deleted in 30 minutes.",
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await sent_message.reply_text(
+            "Thank you for waiting! Your file is ready and will be deleted in 30 minutes.",
+            disable_web_page_preview=True,
+            quote=True
+        )
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
