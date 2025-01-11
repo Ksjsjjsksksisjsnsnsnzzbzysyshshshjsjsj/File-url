e16b651ae1238c2a77a92f39960e80b3abe0b9a1
rohit3607 <rohitplayer87089@gmail.com>
2025-01-11 22:14:36 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -4,6 +4,7 @@ import string
 import random
 from configs import Config
 from pyrogram import Client
+from pyrogram.enums import ParseMode, ChatAction
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
@@ -30,13 +31,26 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
-        await message.delete()
+
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    # Forward the media to the user
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 1800))
 
-async def delete_after_delay(message, delay):
+    # Send a notification message
+    notification_msg = await bot.send_message(
+        chat_id=user_id,
+        text="<b>‼️Forward the Files to Saved Messages or somewhere else before Downloading it.</b>\n<b>It will get Deleted after 30 minutes.‼️</b>",
+        parse_mode=ParseMode.HTML
+    )
+
+    # Schedule deletion after 30 minutes (1800 seconds)
+    asyncio.create_task(delete_after_delay(sent_message, notification_msg, 1800))
+
+async def delete_after_delay(sent_message, notification_msg, delay):
+    # Wait for the specified delay
     await asyncio.sleep(delay)
-    await message.delete()
+
+    # Delete the sent message and notification
+    await sent_message.delete()
+    await notification_msg.delete()
