9d90065d987d46d0ecc3059b0c4001caa577145b
Ksjsjjsksksisjsnsnsnzzbzysyshshshjsjsj <koushalprasad35@gmail.com>
2025-01-12 17:38:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def start(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/qt_movie")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/")
                     ],
                     [
                         InlineKeyboardButton("", callback_data="aboutbot"),
